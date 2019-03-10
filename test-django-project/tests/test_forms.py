381f7bfa5f3bfce727db64a6bf15fb03b4a80b6f
Rafael Henter <henter.r@gmail.com>
2019-03-09 22:35:33 -0300

    Aumentando a cobertura dos testes

--- test-django-project/tests/test_forms.py
+++ test-django-project/tests/test_forms.py
@@ -4,23 +4,32 @@ from django.forms import ValidationError
 from django_stuff.forms import CNPJField, CPFField
 
 
-def test_cpf_field_valid():
-    value = '21552411273'
+@pytest.mark.parametrize('value',
+                         ['21552411273',
+                          '215.524,112-73']
+                         )
+def test_cpf_field_valid(value):
     assert CPFField().clean(value) == value
 
 
-@pytest.mark.parametrize('value', CPFField.invalid_values + ('12345678901',))
+@pytest.mark.parametrize('value',
+                         CPFField.invalid_values + (
+                             '12345678901', '12345678901!@', '1234567890112312312'
+                         ))
 def test_cpf_field_invalid(value):
     with pytest.raises(ValidationError):
         CPFField().clean(value)
 
 
-def test_cnpj_field_valid():
-    value = '09.654.773/0001-66'
+@pytest.mark.parametrize('value',
+                         ['09654773000166',
+                          '09.654.773/0001-66']
+                         )
+def test_cnpj_field_valid(value):
     assert CNPJField().clean(value) == value
 
 
-@pytest.mark.parametrize('value', CNPJField.invalid_values + ('12345678901234',))
+@pytest.mark.parametrize('value', CNPJField.invalid_values + ('12345678901234', '1234567890112312312', '      '))
 def test_cnpj_field_invalid(value):
     with pytest.raises(ValidationError):
         CNPJField().clean(value)
