8e05689a801ca9b2d907dbbd48af74ed34e0c97c
Rafael Henter <henter.r@gmail.com>
2019-03-19 11:19:46 -0300

    Corrige refatoracão

--- django_stuff/utils.py
+++ django_stuff/utils.py
@@ -100,11 +100,11 @@ def validate_cnpj(value):
 
     list1 = list(range(5, 1, -1))
     list2 = list(range(9, 1, -1))
-    new_1dv = sum([i * int(value[idx]) for idx, i in enumerate(list1) + list2])
+    new_1dv = sum([i * int(value[idx]) for idx, i in enumerate(list1 + list2)])
     new_1dv = dv_maker(new_1dv % 11)
     value = value[:-2] + str(new_1dv) + value[-1]
     list3 = list(range(6, 1, -1))
-    new_2dv = sum([i * int(value[idx]) for idx, i in enumerate(list3) + list2])
+    new_2dv = sum([i * int(value[idx]) for idx, i in enumerate(list3 + list2)])
     new_2dv = dv_maker(new_2dv % 11)
     value = value[:-1] + str(new_2dv)
     if value[-2:] != orig_dv:
