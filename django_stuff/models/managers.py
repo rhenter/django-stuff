9ba75804b556def94c865037d01bdc1af47c1df2
Rafael Henter <henter.r@gmail.com>
2019-11-11 14:08:07 -0300

    add fix tests and remove manager

--- django_stuff/models/managers.py
+++ django_stuff/models/managers.py
@@ -1,16 +1,5 @@
 from django.db import models
-from django.db import transaction
 
 
 class SignalsManager(models.Manager):
-
-    def create(self, **kwargs):
-        model_instance = self.initialize_model_instance(**kwargs)
-        with transaction.atomic():
-            model_instance.trigger_event('pre_save', {'is_creation': True})
-            model_instance.save()
-            model_instance.trigger_event('post_save', {'is_creation': False})
-        return model_instance
-
-    def initialize_model_instance(self, **kwargs):
-        return self.model(**kwargs)
+    pass
