1f9b1150e4c87c4bdc353e36dfac5693396533c8
rafaelhenter <rafael@henter.com.br>
2020-09-24 12:01:25 -0300

    Fix Signal Model Create

--- django_stuff/models/managers.py
+++ django_stuff/models/managers.py
@@ -9,9 +9,7 @@ class SignalsManager(models.Manager):
     def create(self, **kwargs):
         model_instance = self.initialize_model_instance(**kwargs)
         with transaction.atomic():
-            model_instance.trigger_event('pre_save', {'is_creation': True})
             model_instance.save()
-            model_instance.trigger_event('post_save', {'is_creation': False})
         return model_instance
 
     def initialize_model_instance(self, **kwargs):
