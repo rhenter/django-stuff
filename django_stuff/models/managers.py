fe1c098d548cc761f53b93f73dfafa982eef5daa
rafaelhenter <rafael@henter.com.br>
2020-09-30 11:20:39 -0300

    Update delete with force hard_delete

--- django_stuff/models/managers.py
+++ django_stuff/models/managers.py
@@ -1,7 +1,9 @@
-from django.db import models
-from django.db import transaction
+from django.db import models, transaction
 from django.db.models import QuerySet
+from django.db.models.deletion import Collector
 from django.utils import timezone
+
+REPR_OUTPUT_SIZE = 20
 
 
 class SignalsManager(models.Manager):
@@ -21,13 +23,42 @@ class SoftDeleteQuerySet(QuerySet):
         return super().update(deleted_at=timezone.now(), is_deleted=True)
 
     def hard_delete(self):
-        return super().delete()
+        """Delete the records in the current QuerySet."""
+        self._not_support_combined_queries('delete')
+        assert not self.query.is_sliced, \
+            "Cannot use 'limit' or 'offset' with delete."
+
+        if self._fields is not None:
+            raise TypeError("Cannot call delete() after .values() or .values_list()")
+
+        del_query = self._chain()
+
+        # The delete is actually 2 queries - one to find related objects,
+        # and one to delete. Make sure that the discovery of related
+        # objects is performed on the same database as the deletion.
+        del_query._for_write = True
+
+        # Disable non-supported fields.
+        del_query.query.select_for_update = False
+        del_query.query.select_related = False
+        del_query.query.clear_ordering(force_empty=True)
+
+        collector = Collector(using=del_query.db)
+        collector.collect(del_query)
+        deleted, _rows_count = collector.delete()
+
+        # Clear the result cache, in case this QuerySet gets reused.
+        self._result_cache = None
+        return deleted, _rows_count
 
     def restore(self):
         return super().update(deleted_at=None, is_deleted=False)
 
-    def trash(self):
-        return self.filter(is_deleted=True)
+    def __repr__(self):
+        data = list(self[:REPR_OUTPUT_SIZE + 1])
+        if len(data) > REPR_OUTPUT_SIZE:
+            data[-1] = "...(remaining elements truncated)..."
+        return '<QuerySet %r>' % data
 
 
 class SoftDeleteSignalsManager(SignalsManager):
@@ -54,3 +85,6 @@ class SoftDeleteSignalsManager(SignalsManager):
             qs = SoftDeleteQuerySet(self.model, using=self._db)
             return qs.filter(*args, **kwargs)
         return super().filter(*args, **kwargs)
+
+    def trash(self):
+        return self.filter(is_deleted=True)
